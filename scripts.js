925165d4ad9e6872c3f9bfffaa0cf0578da378f4
pottsaf <aaronfpotts@gmail.com>
2023-03-22 08:43:24 -0600

    Change grid style from external to inline

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('grid-container');
 
-for (i = 0; i < 1024; i++) {
+
+for (i = 0; i < 16**2; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     container.appendChild(gridItem);
