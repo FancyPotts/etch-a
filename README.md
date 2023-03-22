5136f16436c187bc2ad2790d95faec7d938e8393
pottsaf <aaronfpotts@gmail.com>
2023-03-22 16:37:52 -0600

    Modify placeholder and update reset button to empty input

--- README.md
+++ README.md
@@ -5,4 +5,8 @@
 - &#x2611; Add mouseover event
 - &#x2611; Change grid from external style sheet to inline
     - This is so it can be modified with ease
+- &#x2611; Add buttons
+- &#x2611; Add Reset
+- &#x2611; Change to input instead of arbitary set numbers
+- &#x2611; Added safeguard to keep number from exceeding 100
 - 
--- scripts.js
+++ scripts.js
@@ -1,35 +1,43 @@
-const default_size = 16
-const container = document.getElementById('grid-container');
-const resetBtn = document.getElementById('reset')
-
-const changeSize = document.getElementById('change');
-
-resetBtn.addEventListener('click', function(){ grid(default_size);});
-changeSize.addEventListener('click', function(){ 
-    let sizeInput = document.getElementById('size').value;
-    grid(sizeInput)
-});
-
+const default_size = 16;
+const container = document.getElementById("grid-container");
+const resetBtn = document.getElementById("reset");
+const changeSize = document.getElementById("change");
 
 function grid(size) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
-
-    for (i = 0; i < size**2; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        container.appendChild(gridItem);
-        gridItem.addEventListener("mouseover", function() {
-            gridItem.classList.add('black');
-        })
+  for (i = 0; i < size ** 2; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    container.appendChild(gridItem);
+    gridItem.addEventListener("mouseover", function () {
+      gridItem.classList.add("black");
+    });
+  }
 }
-}
 
+resetBtn.addEventListener("click", function () {
+  grid(default_size);
+  document.getElementById("size").value = "";
+});
+
+changeSize.addEventListener("click", function () {
+  let sizeInput = document.getElementById("size").value;
+  if (sizeInput > 100) {
+    alert("This is too much, keep it at 100 or below.");
+    return;
+  } else if (sizeInput <= 0) {
+    alert("That won't do anything.");
+    return;
+  } else {
+    grid(sizeInput);
+  }
+});
 
 window.onload = () => {
-    grid(default_size)
-}
+  grid(default_size);
+};
