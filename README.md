4ae167f9fdc6f4283c6a22956333ce21d088adf8
pottsaf <aaronfpotts@gmail.com>
2023-03-22 08:58:20 -0600

    Change grid to load via javascript with default

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 - &#x2611; Create a 16 by 16 grid
 - &#x2611; Modify to 32 by 32 grid
 - &#x2611; Add mouseover event
+- &#x2611; Change grid from external style sheet to inline
+    - This is so it can be modified with ease
 - 
--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
+const default_size = 16
 const container = document.getElementById('grid-container');
 
+function grid (size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-for (i = 0; i < 16**2; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    container.appendChild(gridItem);
-    gridItem.addEventListener("mouseover", function() {
-        gridItem.classList.add('black');
-    })
+    for (i = 0; i < 16**2; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        container.appendChild(gridItem);
+        gridItem.addEventListener("mouseover", function() {
+            gridItem.classList.add('black');
+        })
+}
 }
 
 const onGrid = document.querySelector("div");
 
-;
+window.onload = () => {
+    grid(default_size)
+}
