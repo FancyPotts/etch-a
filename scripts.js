43d6b10a4da525bb57c60c5283756ece25243700
pottsaf <aaronfpotts@gmail.com>
2023-03-21 18:47:50 -0600

    Modify to 32 by 32 grid and add border

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('grid-container');
 
-for (i = 0; i < 256; i++) {
+for (i = 0; i < 1024; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     container.appendChild(gridItem);
